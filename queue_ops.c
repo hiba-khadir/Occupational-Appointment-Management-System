2e0db5a4ec6c89a3e11ca25fa25f65874474956d
hiba_khadir <khadirhiba0745@gmail.com>
2025-03-11 22:21:04 +0100

    updated the queue  and lists models

--- queue_ops.c
+++ queue_ops.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "queue_ops.h"
 
 
@@ -10,12 +11,12 @@
 
 
 /*structure of a linked lists cell will be used  as a queue*/
-/*type of an element in the list*/
-typedef struct cell cell;     
+
+typedef struct cell typeCell;   /*type of an element in the list*/   
 
 struct cell {          
-    consultation conslt;
-    cell *addr;
+    consultation conslt;            
+    typeCell *addr;                 /*address of next*/
 };
 
 
@@ -25,8 +26,8 @@ typedef struct typeQueue typeQueue ;  /*define queue as a type */
 
 struct typeQueue
 {
-    cell *h ;            /*pointer to the head of the queue*/
-    cell *t ;            /*pointer to the head of the queue*/
+    typeCell *h ;                   /*pointer to the head of the queue*/
+    typeCell *t ;                   /*pointer to the head of the queue*/
 }; 
 
 
@@ -63,28 +64,53 @@ typedef struct consultation consultation ;
 /*-------------------------the linked lists model implementation----------------------------*/
 /*same linked list model , the type of the element in the cell is a consultation*/
 
-void Allocate(cell **p){
-    *p = malloc(sizeof(cell));
+void Allocate(typeCell **p){
+    *p = malloc(sizeof(typeCell));
 }
 
-cell* Next(cell *p){
+typeCell* Next(typeCell *p){
     return p->addr ;
 }
 
 
-void Ass_consultation(cell *p , consultation c){
-    (p->conslt).
+void Ass_consultation(typeCell **k , consultation c){
+    int i , j ;         /*to iterate through strings*/
+
+    /*copy each field from c consultation to the cell */
+    (*k)->conslt.Consultation_Reason = c.Consultation_Reason ;
+    (*k)->conslt.Employee_Name = c.Employee_Name ;
+
+    strcpy((*k)->conslt.Consultation_Time , c.Consultation_Time);
+    strcpy((*k)->conslt.Employee_ID, c.Employee_ID ) ;
+    
 }
 
 
-void Ass_addr(cell *p , cell *q){
-    p->addr = q ;
+void Ass_addr(typeCell **p , typeCell *q){
+    (*p)->addr = q ;
+
 }
 
-/*
-int value(cell *p){
-    return p->value ;
-}*/
+
+
+
+void display_consultation(typeCell *k){
+    char *name = k->conslt.Employee_Name ; 
+    
+    printf("Employee ID : %s \n", k->conslt.Employee_ID);
+
+    printf("Employee Name : ");
+
+    while(*name != '\0'){            /*display the name letter by letter*/
+        printf("%c",*name);
+        name++ ;
+    }
+    printf("\n");
+
+    printf("Consultation Time : %s \n", k->conslt.Consultation_Time);
+    printf("Concultation Reason : %s" , k->conslt.Consultation_Reason);
+
+}
 
 
 /*------------------------------------------------------------------------------------------------*/
@@ -107,9 +133,13 @@ int emptyQueue(typeQueue Q){
 
 
 void enqueue(typeQueue *Q , consultation new_conslt){
-    cell *new ; 
-    Allocate(new);                     /*allocate a new cell in the queue (list)*/
-    new->conslt = new_conslt ;         /*assign the consultation*/
+    typeCell *new ; 
+
+
+    Allocate(&new);                               /*allocate a new cell in the queue (list)*/
+    Ass_consultation(&new , new_conslt);          /*assign the consultation*/
+    Ass_addr(&new ,NULL);                         /*new is at the tail*/
+
 
 
     if (!emptyQueue(*Q))   
@@ -118,7 +148,16 @@ void enqueue(typeQueue *Q , consultation new_conslt){
         (*Q).t = (*Q).t -> addr ;       /*move the tail one element left*/
         
     }
+    else       /*new is the first in the queue*/
+    {
+        Q->h = new ;
+        Q->t = new ; 
+    }
+    
     
 } 
+void dequeue(typeQueue *Q , consultation *dequeued_conslt ){
+    
+}
 
 
